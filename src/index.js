067839d30328d95e695f70f19f826159fd42a9b8
JonatanDrysen <jojodrysen@gmail.com>
2024-01-16 14:02:27 +0100

    clarify console logs

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ const numbers = [2, 6, 12, 7, 22, 35]
 
 // task 1
 const firstValue = numbers.at(0) // numbers[0] also works
-console.log(firstValue)
+console.log("task 1: ", firstValue)
 
 // task 2
 const lastValue = numbers.at(-1)
-console.log(lastValue)
+console.log("task 2: ", lastValue)
 
 // task 3
 const fourthValue = numbers.at(3)
-console.log(fourthValue)
+console.log("task 3: ", fourthValue)
 
 // task 4
 const secondAndFifthArr = numbers.slice(1, 2).concat(numbers.slice(4, 5))
 // using slice() since it makes a shallow copy and doesn't
 // mutate the original array, unlike splice()
-console.log(secondAndFifthArr)
+console.log("task 4: ", secondAndFifthArr)
 
 // task 5
 const numbersLength = numbers.length
-console.log(numbersLength)
+console.log("task 5", numbersLength)
