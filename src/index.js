fc5d8e993b6d27e80dc6af4aef3456330e106b2d
JonatanDrysen <jojodrysen@gmail.com>
2024-01-16 13:54:27 +0100

    add task 5

--- src/index.js
+++ src/index.js
@@ -14,5 +14,10 @@ console.log(fourthValue)
 
 // task 4
 const secondAndFifthArr = numbers.slice(1, 2).concat(numbers.slice(4, 5))
-// using slice() since it makes a shallow copy and doesn't mutate the original array.
+// using slice() since it makes a shallow copy and doesn't
+// mutate the original array, unlike splice()
 console.log(secondAndFifthArr)
+
+// task 5
+const numbersLength = numbers.length
+console.log(numbersLength)
