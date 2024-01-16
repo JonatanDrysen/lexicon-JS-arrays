67e48fcc1973e428e0594c220d51e0a1b6fe4481
JonatanDrysen <jojodrysen@gmail.com>
2024-01-16 13:36:32 +0100

    add task 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const numbers = [2, 6, 12, 7, 22, 35]
 
 // task 1
-const firstValue = numbers[0]
+const firstValue = numbers.at(0) // numbers[0] also works
 console.log(firstValue)
 
+// task 2
+const lastValue = numbers.at(-1)
+console.log(lastValue)
